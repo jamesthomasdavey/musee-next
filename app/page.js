e5e932714eb7fae3ac3e994664366ed0b24ab1f2
James Davey <james.t.davey@gmail.com>
2024-05-26 11:29:24 -0700

    reinstall nextjs without src

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Begin by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Get started by editing&nbsp;
+          <code className={styles.code}>app/page.js</code>
         </p>
         <div>
           <a
