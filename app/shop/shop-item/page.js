c0f322712ffa271050f2f8a774f02cd9ea66a184
James Davey <james.t.davey@gmail.com>
2024-05-27 10:36:44 -0700

    set up main pages

---
+++ app/shop/shop-item/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShopItemPage = () => {
+    return (
+        <div>ShopItemPage</div>
+    )
+}
+
+export default ShopItemPage
---
+++ app/visit/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VisitPage = () => {
+    return (
+        <div>VisitPage</div>
+    )
+}
+
+export default VisitPage
